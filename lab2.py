9ad3f01c49a84f794b124a6a75105032a52e57c1
anaylo2311 <apnaylor0@frostburg.edu>
2026-02-11 15:41:58 -0500

    started lab 3

--- lab2.py
+++ lab2.py
@@ -1,1 +1,24 @@
 #this will be the code fo the 900 dice rolls and the bar graph for it
+import random
+import matplotlib.pyplot as plt
+def roll_dice(num_rolls):
+    results = []
+    for _ in range(num_rolls):
+        roll = random.randint(1, 6)
+        results.append(roll)
+    return results
+
+def getBarPlot(results):
+    counts = [0] * 6
+    for result in results:
+        counts[result - 1] += 1
+
+    plt.bar(range(1, 7), counts)
+    plt.xlabel('Dice Face')
+    plt.ylabel('Frequency')
+    plt.title('Dice Roll Frequencies')
+    plt.xticks(range(1, 7))
+    plt.show()
+
+results = roll_dice(900)
+getBarPlot(results)
